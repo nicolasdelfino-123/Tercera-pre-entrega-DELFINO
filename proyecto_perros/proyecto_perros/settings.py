d3fe521719e22ab7d138ce0e534717604da47047
nicolasdelfino-123 <nicolasdelfino585@gmail.com>
2023-05-23 19:47:23 -0300

    solucionado language-code a es-es para que muestre las fechas correctas

--- proyecto_perros/proyecto_perros/settings.py
+++ proyecto_perros/proyecto_perros/settings.py
@@ -107,13 +107,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
@@ -145,3 +146,4 @@ LOGIN_URL = 'login'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
